39262cb1f9e02488a9e6ab203d0a7fba98d0d5b1
Ryan McKinley <ryantxu@gmail.com>
2021-03-10 23:34:40 -0800

    add http output

--- telegraf/plugins/outputs/all/all.go
+++ telegraf/plugins/outputs/all/all.go
@@ -1,5 +1,7 @@
 package all
 
 import (
+	_ "github.com/influxdata/telegraf/plugins/outputs/http"
+	_ "github.com/influxdata/telegraf/plugins/outputs/influxdb"
 	_ "github.com/ryantxu/telegraf-datasource/telegraf/plugins/outputs/grafana-live"
 )
